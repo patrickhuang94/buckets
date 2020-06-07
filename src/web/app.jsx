07281eb02bf19000607ff00db1671ed0433c947b
Patrick Huang <patrickhuang94@gmail.com>
2020-06-07 13:23:46 -0700

    Add new tab for Conference Standing

--- src/web/app.jsx
+++ src/web/app.jsx
@@ -9,6 +9,7 @@ import Login from './onboarding/login'
 import Player from './player'
 import PlayerPage from './player/page'
 import Team from './team'
+import Standing from './standing'
 
 import { store } from './store'
 import normalizeAxios from './services/normalizeAxios'
@@ -48,6 +49,7 @@ function App() {
             <Route exact path="/player" component={Player} />
             <Route exact path="/player/:id" component={PlayerPage} />
             <Route exact path="/team" component={Team} />
+            <Route exact path="/standing" component={Standing} />
           </div>
         </div>
       </Router>

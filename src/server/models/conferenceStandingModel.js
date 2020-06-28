77a736c2e36f71fe7dd48fce2306538384142173
Patrick Huang <patrickhuang94@gmail.com>
2020-06-28 12:51:46 -0700

    Return team logo

--- src/server/models/conferenceStandingModel.js
+++ src/server/models/conferenceStandingModel.js
@@ -1,7 +1,15 @@
 const query = require('../db')
 const Team = require('./teamModel')
 
-async function create({ division, team_id, win, loss, seed, win_loss_percentage, games_back }) {
+async function create({
+  division,
+  team_id,
+  win,
+  loss,
+  seed,
+  win_loss_percentage,
+  games_back,
+}) {
   await query(
     'INSERT INTO "conference_standing" (division, team_id, win, loss, seed, win_loss_percentage, games_back) VALUES ($1, $2, $3, $4, $5, $6, $7)',
     [division, team_id, win, loss, seed, win_loss_percentage, games_back],
@@ -10,11 +18,19 @@ async function create({ division, team_id, win, loss, seed, win_loss_percentage,
 
 async function findAll() {
   const allStandings = await query(
-    `SELECT cs.division, cs.win, cs.loss, cs.seed, cs.win_loss_percentage, cs.games_back, t.name
-      FROM "conference_standing" AS cs
-      JOIN "team" AS t
-      ON cs.team_id = t.id
-      ORDER BY division DESC, cs.seed
+    `SELECT 
+      cs.division, 
+      cs.win, 
+      cs.loss, 
+      cs.seed, 
+      cs.win_loss_percentage, 
+      cs.games_back, 
+      t.name,
+      t.logo
+    FROM "conference_standing" AS cs
+    JOIN "team" AS t
+    ON cs.team_id = t.id
+    ORDER BY division DESC, cs.seed
     `,
   )
 

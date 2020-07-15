1ecb029d78f0f5f00f59b0fb7aa8ab3a657de777
Patrick Huang <patrickhuang94@gmail.com>
2020-07-14 22:29:20 -0700

    Migration to remove team_id foreign key on stats_per_season table

---
+++ src/server/db/migrations/20200715045036-remove-team-id-fk-column-from-stats-per-season-table.js
@@ -0,0 +1,18 @@
+exports.up = function (db) {
+  return db.removeColumn('stats_per_season', 'team_id')
+}
+
+exports.down = function (db) {
+  return db.addColumn('stats_per_season', 'team_id', {
+    type: 'int',
+    foreignKey: {
+      name: 'stats_per_game_team_id_fk',
+      table: 'team',
+      rules: {
+        onDelete: 'CASCADE',
+        onUpdate: 'RESTRICT',
+      },
+      mapping: 'id',
+    },
+  })
+}

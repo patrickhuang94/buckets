25707cfbaf906add2718fd60d877bd4a9c85f2bd
Patrick Huang <patrickhuang94@gmail.com>
2020-07-14 19:03:07 -0700

    Add new `useFetch` hook

--- src/web/hooks/useFetch.js
+++ src/web/hooks/useFetch.js
@@ -1,19 +1,29 @@
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import normalizeAxios from '../services/normalizeAxios'
 
-const useFetch = (url, options, callback) => {
+const cache = {}
+
+const useFetch = (url) => {
+  const [isFetched, setIsFetched] = useState(false)
+  const [data, setData] = useState(null)
+
   useEffect(() => {
     async function fetchData() {
-      const data = await normalizeAxios({
-        method: options.method,
-        url,
-      })
-
-      callback(data)
+      if (cache[url]) {
+        setData(cache[url])
+        setIsFetched(true)
+      } else {
+        const result = await normalizeAxios({ url })
+        cache[url] = result
+        setData(result)
+        setIsFetched(true)
+      }
     }
 
     fetchData()
-  }, [])
+  }, [url])
+
+  return { isFetched, data }
 }
 
 export default useFetch
